3c43718dd7a9fec085fd289eb30410f123639b39
JVSittaG <134104193+JVSittaG@users.noreply.github.com>
2023-10-29 22:47:03 -0300

    cliente_banco.h TESTE
    Versão teste do cliente_banco.h onde esta separado a definição de todas as funções que envolvam o arquivo  "clientesbanco.bin".

--- cliente_banco.h
+++ cliente_banco.h
@@ -1,36 +1,19 @@
-#ifndef CLIENTE_BANCO_H
-#define CLIENTE_BANCO_H
-
-#include <stdio.h>
-#include <time.h>
+#ifndef CLIENTES_H
+#define CLIENTES_H
 
 struct Cliente {
-  char nome[50];
-  char cpf[12];
-  int tipo;
-  double saldo;
-  char senha[20];
+    char nome[50];
+    char cpf[12];
+    int tipo;
+    double saldo;
+    char senha[20];
 };
 
-struct Extrato {
-  char cpf[12];
-  char data[20];
-  double valor;
-  double tarifa;
-  double saldo;
-};
-
-void arquivo_clientes();
-void arquivo_extrato();
-void ler_arquivo_clientes();
-void ler_arquivo_extrato();
 struct Cliente *buscar_cliente(const char *cpf);
 void novo_cliente();
 void apagar_cliente();
 void listar_clientes();
-void debito();
-void deposito();
-void extrato();
-void transferencia();
+void arquivo_clientes();
+void ler_arquivo_clientes();
 
-#endif // CLIENTE_BANCO_H
+#endif
